b1b938d2ef829bf39fdc79fff6c6f2c2c1e7244f
Nitesh Yadav <nitesh.y@zifornd.com>
2024-06-24 23:43:25 +0530

    fix "GET /api/tracks/<track-id>/" and add headers for caching

--- app/routes/tracks.py
+++ app/routes/tracks.py
@@ -2,7 +2,7 @@ import os
 import re
 from app import library
 from http import HTTPStatus as Status
-from flask import Blueprint, request, Response
+from flask import Blueprint, request, Response, send_file
 
 
 track_bp = Blueprint("tracks", __name__)
@@ -15,34 +15,41 @@ def tracks():
 
 @track_bp.route("/<string:track_id>/")
 def track(track_id):
-    track = library.tracks.get(track_id, None)
+    track = library.tracks.get(track_id)
     if track is None:
-        return Response()
-    chunk_size, size = 1024 * 1024, os.path.getsize(track.file)
-    range = re.match(
-        "bytes=(\d+)-(\d*)", request.headers.get("Range", f"bytes=0-{chunk_size-1}")
-    )
-    start, end = int(range.group(1) or 0), int(range.group(2) or size - 1)
+        return Response(status=Status.NOT_FOUND)
+    chunk_size, size = 2 * 1024**2, os.path.getsize(track.file)
+    range = request.headers.get("Range")
+    if range is None:
+        return send_file(track.file)
+    range = re.match("bytes=(\d+)-(\d*)", range)
+    if range is None:
+        return Response(status=Status.BAD_REQUEST)
+    start = int(range.group(1))
+    end = int(range.group(2) or start + chunk_size - 1)
 
-    def get_chunks(start, end):
-        with open(track.file, "rb") as fd:
+    def get_chunks(file, start, end):
+        with open(file, "rb") as fd:
             fd.seek(start)
-            while chunk := fd.read(min(chunk_size, end - start + 1)):
+            while start <= end < size:
+                chunk = fd.read(end - start + 1)
                 yield chunk
+                start += len(chunk)
 
-    readable_size = min(chunk_size, end - start + 1)
     response = Response(
-        get_chunks(start, start + readable_size - 1),
+        get_chunks(track.file, start, end),
         status=Status.PARTIAL_CONTENT,
         content_type=track.mime,
     )
+    read_size = len(response.data)
     response.headers["Content-Range"] = "bytes %d-%d/%d" % (
         start,
-        start + readable_size - 1,
+        start + read_size - 1,
         size,
     )
     response.headers["Accept-Ranges"] = "bytes"
     response.headers["Content-Type"] = track.mime
-    response.headers["Content-Length"] = readable_size
-    start += readable_size
+    response.headers["Content-Length"] = read_size
+    response.headers["ETag"] = os.path.getmtime(track.file)
+    response.headers["Cache-Control"] = "max-age=31536000, immutable"
     return response
