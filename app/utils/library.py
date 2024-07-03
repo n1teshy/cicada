1c66588c96a10d2c3f4e0c1040f44eb6c1317361
Nitesh Yadav <nitesh.y@zifornd.com>
2024-07-03 13:55:49 +0530

    app/utils/library.py -> add 'duration' propety to Track objects, some refactor

--- app/utils/library.py
+++ app/utils/library.py
@@ -3,11 +3,13 @@ import glob
 import hashlib
 import mimetypes
 
-from mutagen.mp3 import MP3
-from mutagen.id3 import ID3
+from mutagen import File
 from app.utils.environment import env
+from app.utils.logger import get_logger
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
+
+logger = get_logger(__name__)
 
 
 def hash_name(name):
@@ -40,13 +42,14 @@ class FSEventHandler(FileSystemEventHandler):
 
 
 class Track:
-    def __init__(self, id, file, mime, title, albums, artists, cover_id=None):
+    def __init__(self, id, file, mime, title, albums, artists, duration, cover_id=None):
         self.id = id
         self.file = file
         self.mime = mime
         self.title = title
         self.albums = albums
         self.artists = artists
+        self.duration = duration
         self.cover_id = cover_id
 
     def to_dict(self):
@@ -55,6 +58,7 @@ class Track:
             "title": self.title,
             "albums": self.albums,
             "artists": self.artists,
+            "duration": self.duration,
             "coverId": self.cover_id,
         }
 
@@ -81,18 +85,23 @@ class Library:
         mime = mimetypes.guess_type(file)[0]
         if not mime or not mime.startswith("audio"):
             return None
+        try:
+            audio = File(file)
+        except:
+            logger.error(f"could not open {file}")
+            return
         track_id = hash_name(file)
         title = os.path.basename(file)
         artists, almbums = [], []
+        duration = audio.info.length
         try:
-            audio = MP3(file, ID3=ID3)
             title = audio.tags["TIT2"].text[0]
             artists = audio.tags["TPE1"].text
             almbums = audio.tags["TALB"].text
             # TODO: add cover image
         except:
             pass
-        track = Track(track_id, file, mime, title, almbums, artists)
+        track = Track(track_id, file, mime, title, almbums, artists, duration)
         self.tracks[track_id] = track
         self.add_track_cb(track)
 
